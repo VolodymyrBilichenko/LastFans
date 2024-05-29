5570624e65c80477f751c746a163625de2b2a961
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2024-05-29 11:45:16 +0300

    add transition

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 import { Route, Routes, useLocation } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 import { routes } from "./routes/routes";
 import { Header } from "./components/Header/Header";
+import { useState } from "react";
 
 import './assets/scss/style.scss'
 
 export const App = () => {
   const location = useLocation();
+  const [loading, setLoading] = useState(false);
+
+  const toggleLoader = (value: any) => {
+    setLoading(value);
+  }
 
   return (
-    <>
+    <div style={{position: 'relative'}}>
+      
+      {loading && <p style={{position: 'absolute', top: '0', left: '0'}}>loading...</p>}
+
       <Header/>
 
-      <Routes location={location}>
-        {
-          routes.map((item: any) => <Route key={item.path} element={item.element} path={item.path}/>)
-        }
-      </Routes>
+      <TransitionGroup component={null}>
+        <CSSTransition key={location.pathname} classNames='fade' timeout={300} onEnter={() => toggleLoader(true)} onExited={() => toggleLoader(false)}>
 
+        <Routes location={location}>
+          {
+            routes.map((item: any) => <Route key={item.path} element={item.element} path={item.path}/>)
+          }
+        </Routes>
 
-    </>
+        </CSSTransition>
+      </TransitionGroup>
+
+    </div>
 
   );
 }
