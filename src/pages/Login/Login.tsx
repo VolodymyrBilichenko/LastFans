69a8670f14bc80deeccbf8ae392f08e9bb0085ed
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2024-06-07 23:01:00 +0300

    update login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,44 +1,54 @@
-import MainDecor from '../../assets/img/registration/01.svg'
-import LogoIc from '../../assets/img/icons/logo.svg'
-import LogoWhiteIc from '../../assets/img/icons/logo-white.svg'
-import GoogleIc from '../../assets/img/icons/google.svg'
-import AppleIc from '../../assets/img/icons/apple.svg'
-import FacebookIc from '../../assets/img/icons/facebook.svg'
-import { NavLink, useNavigate } from 'react-router-dom'
-import { useState } from 'react'
+import MainDecor from '../../assets/img/registration/01.svg';
+import LogoIc from '../../assets/img/icons/logo.svg';
+import LogoWhiteIc from '../../assets/img/icons/logo-white.svg';
+import GoogleIc from '../../assets/img/icons/google.svg';
+import AppleIc from '../../assets/img/icons/apple.svg';
+import FacebookIc from '../../assets/img/icons/facebook.svg';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 export const Login = () => {
     const [password, setPassword] = useState<string>("");
     const [email, setEmail] = useState<string>("");
-    const [error, setError] = useState<string | null>(null);
+    const [emailError, setEmailError] = useState<string | null>(null);
+    const [passwordError, setPasswordError] = useState<string | null>(null);
     const navigate = useNavigate();
 
     const validateForm = () => {
+        let isValid = true;
+
         const emailRegex = /^[^\s@]+@[a-zA-Z0-9\-_.]+\.[a-z]{2,6}$/;
         const isValidEmail = emailRegex.test(email);
-    
+
         if (!isValidEmail) {
-          setError("Введіть дійсні значення");
-          return false;
+            setEmailError("Введіть дійсні значення для електронної пошти");
+            isValid = false;
+        } else {
+            setEmailError(null);
         }
-        return true;
+
+        if (password === '') {
+            setPasswordError("Пароль не може бути порожнім");
+            isValid = false;
+        } else {
+            setPasswordError(null);
+        }
+
+        return isValid;
     };
-    
+
     const resetForm = () => {
         setEmail("");
         setPassword("");
     };
-      
+
     const submitForm = (evt: React.FormEvent<HTMLFormElement>) => {
         evt.preventDefault();
-        setError(null);
-        resetForm();
-        
-        
         if (validateForm()) {
+            resetForm();
             navigate('/');
         }
-    }
+    };
 
     return (
         <div className="registration">
@@ -46,12 +56,10 @@ export const Login = () => {
                 <div className="block-registration__body">
                     <div className="block-registration__logo logo">
                         <div className="logo__block">
-
                             <p className="logo__text">l</p>
                             <img className="logo__image" src={LogoIc} alt="ph"/>
                             <p className="logo__text">st</p>
                         </div>
-
                         <span className="logo__text">fans</span>
                     </div>
                 </div>
@@ -64,22 +72,30 @@ export const Login = () => {
                     <div className="main-registration__header header-main-registration">
                         <h1 className="header-main-registration__title title">Log in</h1>
                     </div>
-                    <form action="" className="main-registration__form form-main-registration"
-                        onSubmit={submitForm}
-                    >
+                    <form className="main-registration__form form-main-registration" onSubmit={submitForm}>
                         <div className="form-main-registration__body">
-                            <input type={'email'} name="form[]" placeholder="Email adress *" className="form-main-registration__input input input-main"
+                            <input 
+                                type="email" 
+                                name="email" 
+                                placeholder="Email address *" 
+                                className="form-main-registration__input input input-main"
+                                value={email}
                                 onChange={(e) => setEmail(e.target.value)}
-                            />{error && <span className="error__message">{error}</span>}
-
-                            <input type="password" name="form[]" placeholder="Password *" className="form-main-registration__input input input-main"
+                            />
+                            {emailError && <span className="error__message" style={{color: '#CB0000'}}>{emailError}</span>}
+                            <input 
+                                type="password" 
+                                name="password" 
+                                placeholder="Password *" 
+                                className="form-main-registration__input input input-main"
+                                value={password}
                                 onChange={(e) => setPassword(e.target.value)}
-                            />{error && <span className="error__message">{error}</span>}
+                            />
+                            {passwordError && <span className="error__message" style={{color: '#CB0000'}}>{passwordError}</span>}
                         </div>
-                        {/* <NavLink to={'/'}> */}
-                            <button className="form-main-registration__button button button--fw"><span>Log in</span></button>
-                        {/* </NavLink> */}
-                        
+                        <button className="form-main-registration__button button button--fw" type="submit">
+                            <span>Log in</span>
+                        </button>
                     </form>
 
                     <NavLink to={'/forgot'} className="main-registration__forgot">Forgot your password?</NavLink>
@@ -116,5 +132,5 @@ export const Login = () => {
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
