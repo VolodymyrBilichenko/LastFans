bd94e63351786b54dbff05b8688a0acba12a5841
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2024-06-08 14:26:50 +0300

    Update Login.tsx

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -14,8 +14,7 @@ export const Login = () => {
     const [passwordError, setPasswordError] = useState<string | null>(null);
     const navigate = useNavigate();
 
-    const YDALIT = [1, 2, 3]
-
+    
     const validateForm = () => {
         let isValid = true;
 
