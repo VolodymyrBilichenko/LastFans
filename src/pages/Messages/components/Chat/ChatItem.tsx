dec4c1fbd20923fba1d020e8672daf2431175c35
Vlas <vlas.zub@gmail.com>
2024-06-09 11:43:05 +0300

    Chat page + change theme

---
+++ src/pages/Messages/components/Chat/ChatItem.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface IChatItemProps {
+    
+}
+
+export const ChatItem:React.FC<IChatItemProps> = () => {
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
