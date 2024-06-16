6d502f6184b1f6606b6209f4f0bb764bcb0381b6
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2024-06-16 15:13:34 +0300

    apple auth soc login

--- src/pages/Login/components/LoginSocial.tsx
+++ src/pages/Login/components/LoginSocial.tsx
@@ -5,6 +5,7 @@ import FacebookIc from '../../../assets/img/icons/facebook.svg';
 
 import { GoogleLogin } from '@react-oauth/google'
 import FacebookLogin from 'react-facebook-login';
+import AppleSignin from 'react-apple-signin-auth';
 
 
 export const LoginSocial = () => {
@@ -15,6 +16,10 @@ export const LoginSocial = () => {
 
     const responseFacebook = (response: any) => {
         console.log('facebook', response.accessToken);
+    }
+
+    const responseApple = (response: any) => {
+        console.log('apple', response.credential);
     }
 
     return (
@@ -33,13 +38,32 @@ export const LoginSocial = () => {
                     />;
                 </div>
             </button>
+
             <button className="socials-main-registration__item item-socials-main-registration">
                 <div className="item-socials-main-registration__image">
                     <img src={AppleIc} alt="Icon" />
                 </div>
                 <p className="item-socials-main-registration__text">Apple</p>
-                
+                <div className='auth'>
+                    <AppleSignin
+                        authOptions={{
+                            clientId: '8FDM6UTX9',
+                            scope: 'email name',
+                            redirectURI: 'https://develop.8FDM6UTX9.com/',
+                            state: '',
+                            nonce: 'nonce',
+                            usePopup: true,
+                        }}
+                        uiType="dark"
+                        className="apple-auth-btn"
+                        onError={(e: any) => {
+                            console.log(e)
+                        }}
+                        onSuccess={responseApple}
+                    />
+                </div>
             </button>
+
             <button className="socials-main-registration__item item-socials-main-registration">
                 <div className="item-socials-main-registration__image">
                     <img src={FacebookIc} alt="Icon" />
