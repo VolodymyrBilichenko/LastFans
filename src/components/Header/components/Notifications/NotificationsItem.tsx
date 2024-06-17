0f9c32831ce56daf644a9f66c6e7184d6017dc59
Vlas <vlas.zub@gmail.com>
2024-06-17 13:21:40 +0300

    Change link "some" to correct

--- src/components/Header/components/Notifications/NotificationsItem.tsx
+++ src/components/Header/components/Notifications/NotificationsItem.tsx
@@ -4,6 +4,7 @@ import dollar from './../../../../assets/img/icons/dollar.svg'
 import clock from './../../../../assets/img/icons/clock.svg'
 import { IUser } from '../../../../models'
 import HTMLReactParser from 'html-react-parser'
+import { NavLink } from 'react-router-dom'
 
 interface INotificationsItemProps {
     user: IUser
@@ -16,21 +17,21 @@ export const NotificationsItem: React.FC<INotificationsItemProps> = ({ user, isR
 
     return (
         <div className={`body-popup-notifications-header__item item-notifications ${!isRead ? "item-notifications--unread" : ""} item-notifications--tip`}>
-            <a href="some" className="item-notifications__image">
+            <button className="item-notifications__image">
                 <img className="user-photo" src={user.photo} alt="ph" />
                 <div className="item-notifications__image-icon">
                     <img src={dollar} alt="ph" />
                 </div>
-            </a>
+            </button>
             <div className="item-notifications__body body-item-notifications">
-                <div className="body-item-notifications__top">
-                    <a href="some" className="body-item-notifications__name">
+                <NavLink to={`/profile/${user.usertag}`} className="body-item-notifications__top">
+                    <p className="body-item-notifications__name">
                         {user.username}
-                    </a>
+                    </p>
                     <p className="body-item-notifications__username">
                         {user.usertag}
                     </p>
-                </div>
+                </NavLink>
                 <div className="body-item-notifications__content content-body-item-notifications">
                     <p className="content-body-item-notifications__text">
                         {HTMLReactParser(message ?? "")}
